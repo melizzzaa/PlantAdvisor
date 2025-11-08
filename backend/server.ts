02373b13480fcb7f10a12394160a233d4e7d13e8
Melissa Krömpel <melissa.kroempel@outlook.de>
2025-11-08 01:36:45 +0100

    deno und oak installiert und lauffähig

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/api/test", (ctx) => {
+  ctx.response.body = {
+    message: "Backend geht",
+    time: new Date().toISOString(),
+  };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Backend läuft auf http://localhost:8000");
+await app.listen({ port: 8000 });
