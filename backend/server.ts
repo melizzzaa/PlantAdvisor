6bf7d754798511fd676f69fbe77cf54e851e1d34
Melissa Krömpel <melissa.kroempel@outlook.de>
2025-11-16 18:22:09 +0100

    post route für das anlegen der pflanzen

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { listPlants} from "./lib/plantService.ts";
 
 const app = new Application();
 const router = new Router();
@@ -10,8 +11,16 @@ router.get("/api/test", (ctx) => {
   };
 });
 
+router.get("/api/plants", async (ctx) => {
+  const plants = await listPlants();
+  ctx.response.body = plants;
+});
+
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+console.log("SERVER: richtige Datei läuft!");
 console.log("Backend läuft auf http://localhost:8000, test unter http://localhost:8000/api/test");
 await app.listen({ port: 8000 });
+
