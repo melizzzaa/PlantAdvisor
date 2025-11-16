15c431363b4bcbb0c356fc918e16dd8ae73532d7
Melissa Krömpel <melissa.kroempel@outlook.de>
2025-11-17 00:30:19 +0100

    einfache pflanzenliste eingebaut

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { ref, onMounted } from "vue";
+
+const plants = ref([]);
+
+onMounted(async () => {
+  const res = await fetch("http://localhost:8000/api/plants");
+  plants.value = await res.json();
+});
 </script>
 
 <template>
+  <h1>Pflanzenliste</h1>
+
+  <ul>
+    <li v-for="p in plants" :key="p.id">
+      {{ p.name }} ({{ p.plantType }})
+    </li>
+  </ul>
 </template>
 
 <style>
